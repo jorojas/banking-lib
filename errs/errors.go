2b6448b5dcd3a87736706b0853fe6597199542c0
Jonathan Rojas <jorojas@novopayment.com>
2023-12-29 16:44:11 -0500

    Extracted the error and logger from banking api

---
+++ errs/errors.go
@@ -0,0 +1,39 @@
+package errs
+
+import (
+	"net/http"
+)
+
+type AppError struct {
+	Code    int    `json:",omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+func (e AppError) AsMessage() *AppError {
+	return &AppError{
+		Message: e.Message,
+	}
+}
+
+func NewNotFoundError(message string) *AppError {
+	return newGenericError(message, http.StatusNotFound)
+}
+
+func NewUnexpectedError(message string) *AppError {
+	return newGenericError(message, http.StatusInternalServerError)
+}
+
+func NewBadRequestError(message string) *AppError {
+	return newGenericError(message, http.StatusBadRequest)
+}
+
+func NewValidationError(message string) *AppError {
+	return newGenericError(message, http.StatusUnprocessableEntity)
+}
+
+func newGenericError(message string, code int) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    code,
+	}
+}
